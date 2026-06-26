48bf262b55792099fae979673e2705ae8b0897e2
Nikitha <2103a52154@sru.edu.in>
2026-06-26 10:28:55 +0530

    chore: remove stray debug/test/temp files from project root and backend

--- backend-fastapi/get_pid.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine, text
-engine = create_engine('postgresql://postgres:Nikitha@localhost/DPR')
-with engine.connect() as conn:
-    res = conn.execute(text("SELECT id, project_name FROM projects WHERE project_name ILIKE '%AHEJ5L PSS-05%'"))
-    print(list(res))
